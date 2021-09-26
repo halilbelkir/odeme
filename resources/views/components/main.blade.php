b63b5c8680ff58c9746e8cac8a7487c5e492fac9
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2021-09-26 13:37:09 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -17,7 +17,7 @@
 <body class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Main navigation">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">{!! Media::createTag('assets/img/logo-beyaz.png',['width' =>[107], 'height' => [30]]) !!}</a>
+            <a class="navbar-brand" href="{{route('price.list')}}">{!! Media::createTag('assets/img/logo-beyaz.png',['width' =>[107], 'height' => [30]]) !!}</a>
             <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -27,10 +27,11 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{route('price.list')}}">Kalan Taksitler</a>
                     </li>
+
+                    <!--
                     <li class="nav-item">
                         <a class="nav-link" href="#">Geçmiş Taksitler</a>
                     </li>
-                    <!--
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown01">
