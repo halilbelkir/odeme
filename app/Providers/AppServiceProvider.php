f5f19b659e656decf93a8bec56022f663ebfdf2a
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2021-09-29 21:17:04 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,43 +29,41 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('*',function($view)
         {
-            if (Cache::has('remainder'))
+            if (Auth::user())
             {
-                $remainder =  Cache::get('remainder');
-            }
-            else
-            {
-                $customerCode = Auth::user()->customer_code;
-                //$customerCode = 246492;
-                $remainder    = DB::connection('sqlsrv')->select('Exec sp_mus_odeme :customer_code',['customer_code' => $customerCode]);
-                Cache::put('remainder', $remainder, Carbon::now()->addMinutes(480));
-            }
-
-            if (Cache::has('priceList'))
-            {
-                $priceList  = Cache::get('priceList');
-                $totalPrice = Cache::get('totalPrice');
-            }
-            else
-            {
-                $priceList  = array();
-                $totalPrice = 0;
-
-                foreach ($remainder as $price)
-                {
-                    $key = $price->Month;
-                    $priceList[$key]['price'] = isset($priceList[$key]) ? $priceList[$key]['price'] + $price->RemainingInstallment : $price->RemainingInstallment;
-                    $priceList[$key]['month'] = $price->AYAD1;
-                    $priceList[$key]['year']  = $price->YIL;
-                    $totalPrice += $price->RemainingInstallment;
+                if (Cache::has('remainder')) {
+                    $remainder = Cache::get('remainder');
+                }
+                else {
+                    $customerCode = Auth::user()->customer_code;
+                    //$customerCode = 246492;
+                    $remainder = DB::connection('sqlsrv')->select('Exec sp_mus_odeme :customer_code', ['customer_code' => $customerCode]);
+                    Cache::put('remainder', $remainder, Carbon::now()->addMinutes(480));
                 }
 
-                Cache::put('priceList', $priceList, Carbon::now()->addMinutes(480));
-                Cache::put('totalPrice', $totalPrice, Carbon::now()->addMinutes(480));
-            }
+                if (Cache::has('priceList')) {
+                    $priceList = Cache::get('priceList');
+                    $totalPrice = Cache::get('totalPrice');
+                }
+                else {
+                    $priceList = array();
+                    $totalPrice = 0;
 
-            $view->with('priceList', $priceList);
-            $view->with('totalPrice', $totalPrice);
+                    foreach ($remainder as $price) {
+                        $key = $price->Month;
+                        $priceList[$key]['price'] = isset($priceList[$key]) ? $priceList[$key]['price'] + $price->RemainingInstallment : $price->RemainingInstallment;
+                        $priceList[$key]['month'] = $price->AYAD1;
+                        $priceList[$key]['year'] = $price->YIL;
+                        $totalPrice += $price->RemainingInstallment;
+                    }
+
+                    Cache::put('priceList', $priceList, Carbon::now()->addMinutes(480));
+                    Cache::put('totalPrice', $totalPrice, Carbon::now()->addMinutes(480));
+                }
+
+                $view->with('priceList', $priceList);
+                $view->with('totalPrice', $totalPrice);
+            }
         });
 
     }
