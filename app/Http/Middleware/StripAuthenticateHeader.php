451b8223d58c3646fd28f6cbf737b27fa7e34188
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2022-06-24 14:00:20 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- app/Http/Middleware/StripAuthenticateHeader.php
+++ app/Http/Middleware/StripAuthenticateHeader.php
@@ -20,13 +20,9 @@ class StripAuthenticateHeader {
      * @param  \Closure                 $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('X-Content-Type-Options', 'nosniff');
-        $response->headers->set('X-XSS-Protection', '1; mode=block');
-        $response->headers->set('Strict-Transport-Security', 'max-age:31536000; includeSubDomains');
-        $response->headers->set('Public-Key-Pins', 'pin-sha256="base64=' . env('SUBJECT_PUBLIC_KEY_INFORMATION_FINGERPRINT') . '"; max-age=31536000; includeSubDomains');
         $this->removeUnwantedHeaders($this->unwantedHeaderList);
         return $response;
     }
