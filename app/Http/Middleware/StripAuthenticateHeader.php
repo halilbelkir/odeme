c681bbdc8f6227f69ba31abc61487e07bad42fe6
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2022-06-24 13:58:31 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- app/Http/Middleware/StripAuthenticateHeader.php
+++ app/Http/Middleware/StripAuthenticateHeader.php
@@ -7,9 +7,36 @@ use Illuminate\Http\Request;
 
 class StripAuthenticateHeader {
 
+    // Enumerate unwanted headers
+    private $unwantedHeaderList = [
+        'X-Powered-By',
+        'Server',
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-        $request->headers->remove('www-authenticate');
-        return $next($request);
+        $response = $next($request);
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('Strict-Transport-Security', 'max-age:31536000; includeSubDomains');
+        $response->headers->set('Public-Key-Pins', 'pin-sha256="base64=' . env('SUBJECT_PUBLIC_KEY_INFORMATION_FINGERPRINT') . '"; max-age=31536000; includeSubDomains');
+        $this->removeUnwantedHeaders($this->unwantedHeaderList);
+        return $response;
+    }
+
+    /**
+     * @param $headerList
+     */
+    private function removeUnwantedHeaders($headerList)
+    {
+        foreach ($headerList as $header)
+            header_remove($header);
     }
 }
